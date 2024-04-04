d7d7154bcc3588fd0fe4f851d4c6cce9a755550f
Vansh Sethi <128701049+vanshsethi23@users.noreply.github.com>
2024-04-04 09:47:23 +0530

    Update FirstJavaProgram.java

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -7,5 +7,9 @@ public class FirstJavaProgram {
     public static void main(String[] args) {
         // Write a Program of Hello World using Command Prompt in Java Programming Language.
         System.out.println("Hello World");
+
+        // println will display the message on a new line, while the print statement will print the message on the same line.
+        // print("string") - doesn't give a line space in the output.
+        // println("string") - gives a line space in the output.
     }
 }
