b7d1d848a21ec0c2b83a3fc4da84da9bd8822f98
Vansh Sethi <vanshsethi.usar@gmail.com>
2024-04-04 11:22:27 +0530

    added mathclass in Java notes&program

---
+++ MathClass.java
@@ -0,0 +1,16 @@
+package com.sampleproject;
+
+public class mathclass {
+    public static void main(String[] args) {
+        // Maths Class - this class in JAVA provides a set of static methods for performing common mathematical operations.
+        System.out.println(Math.max(3894, 3891));
+        System.out.println(Math.min(756, 789));
+
+        System.out.println();
+        
+        // Math.random(): Returns a pseudo-random double between 0.0 (inclusive) and 1.0 (exclusive). Prints "long" values by default.
+        System.out.println(Math.random());
+        System.out.println((int)(Math.random()*100));
+        System.out.println(Math.pow(3,2));
+    }
+}
